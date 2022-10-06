348afabf34157fe65a4905d00bf3a369dbfa3f21
Connor Crowe <connor.haz.crowe@gmail.com>
2022-10-06 16:16:19 -0400

    Add post request for leetcode data and some early date formatting. GH and LC objects still incompatible

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const pretty = require('pretty');
-const fs = require('fs');
+const fetch = require("node-fetch");
 
-const URL_GITHUB = '';
+const URL_GITHUB = 'https://github.com/connorcrowe';
 const URL_GITLAB = '';
-const URL_LEETCODE = '';
+const URL_LEETCODE = 'https://leetcode.com/connorthecrowe/';
 
-const USE_GITHUB = false;
+const USE_GITHUB = true;
 const USE_GITLAB = false;
 const USE_LEETCODE = true;
 
@@ -40,43 +39,55 @@ async function scrapeGitLab(url, inData) {
 async function scrapeLeetCode(url, inData) {
     outData = inData;
 
-    try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
+    const data = await fetch("https://leetcode.com/graphql/", {
+        "credentials": "include",
+        "headers": {
+            "Accept": "*/*",
+            "Accept-Language": "en-CA,en-US;q=0.7,en;q=0.3",
+            "content-type": "application/json",
+            "authorization": "",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
+        },
+        "referrer": url,
+        "body": "{\"query\":\"\\n    query userProfileCalendar($username: String!, $year: Int) {\\n  matchedUser(username: $username) {\\n    userCalendar(year: $year) {\\n      activeYears\\n      streak\\n      totalActiveDays\\n      dccBadges {\\n        timestamp\\n        badge {\\n          name\\n          icon\\n        }\\n      }\\n      submissionCalendar\\n    }\\n  }\\n}\\n    \",\"variables\":{\"username\":\"connorthecrowe\"}}",
+        "method": "POST",
+        "mode": "cors"
+    }).then(console.log('fetched')); 
 
-        let listContributionDays = $("*");
-        let a = listContributionDays[0]
-        fs.writeFile('/Output.txt', $.html(), (err) => {
-      
-            // In case of a error throw err.
-            if (err) throw err;
-        })
-        //console.log(pretty($.html()));
-        //for (let i=0; i<listContributionDays.length; i++) {
-        //    outData[listContributionDays[i]['attribs']['data-date']] = {'github': listContributionDays[i]['attribs']['data-level']};
-        //}
-    } catch (err) {
-        console.error(err);
+    const jsonData = await data.json();
+    const calendar = JSON.parse(jsonData.data.matchedUser.userCalendar.submissionCalendar);
+
+    for (unixDate in calendar) {
+       // console.log(date, calendar[date])
+       //const date = new Date(unixDate  * 1000);
+       console.log(formatDate(unixDate));
     }
-
+    //inData['2022-10-06']['leet'] = '1'
+    //console.log(inData['2022-10-06'])
     return outData;
+}
+
+function formatDate(unixDate) {
+    const tempDate = new Date(unixDate * 1000);
+    return [
+        tempDate.getFullYear(),
+        (tempDate.getMonth()+1).toString().padStart(2, '0'),
+        tempDate.getDate().toString().padStart(2, '0'),
+    ].join('-');
 }
 
 async function main() {
     unifiedData = initializeStructure();
 
-    if (USE_GITHUB) {
-        unifiedData = await scrapeGitHub(URL_GITHUB, unifiedData);
-    }
+    if (USE_GITHUB) unifiedData = await scrapeGitHub(URL_GITHUB, unifiedData);
 
-    if (USE_GITLAB) {
-        unifiedData = await scrapeGitLab(URL_GITLAB, unifiedData);
-    }
+    if (USE_GITLAB) unifiedData = await scrapeGitLab(URL_GITLAB, unifiedData);
+    
+    if (USE_LEETCODE) unifiedData = await scrapeLeetCode(URL_LEETCODE, unifiedData);
+    
 
-    if (USE_LEETCODE) {
-        unifiedData = await scrapeLeetCode(URL_LEETCODE, unifiedData)
-    }
-
-    console.log(unifiedData);
+   // console.log(unifiedData);
 }
 main();
