2029152feedc0de614ebb2f456e812fec8870102
connorcrowe <connor.haz.crowe@gmail.com>
2022-10-05 20:04:18 -0400

    set up structure

---
+++ main.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const URL_GITHUB = '';
+const URL_GITLAB = '';
+const URL_LEETCODE = '';
+
+async function initializeStructure() {
+    return {};
+}
+
+async function scrapeGitHub(url) {
+    return {};
+}
+
+async function scrapeGitLab(url) {
+    return {};
+}
+
+async function scrapeLeetCode(url) {
+    return {}
+}
+
+unifiedData = initializeStructure();
