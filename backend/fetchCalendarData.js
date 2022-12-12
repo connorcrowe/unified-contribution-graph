8f2f66980a35218f8424811bceb38131383e4bbf
Connor Crowe <connor.haz.crowe@gmail.com>
2022-12-12 17:02:24 -0500

    Add initialization of data structure to send to frontend - need to populate still

--- backend/fetchCalendarData.js
+++ backend/fetchCalendarData.js
@@ -6,4 +6,27 @@ exports.fetchData = () => {
     };
     return responseData;
 }
-//modules.export = { fetchData };
+
+function jsDateToDashForm (jsDate) {
+    return [
+        jsDate.getFullYear(),
+        (jsDate.getMonth()+1).toString().padStart(2, '0'),
+        jsDate.getDate().toString().padStart(2, '0'),
+    ].join('-');
+}
+
+function initializeContributionObject() {
+    outData = []
+    const jsDate = new Date();
+    jsDate.setHours(0,0,0,0)
+    jsDate.setDate(jsDate.getDate()-365);
+
+    for (let i = 0; i < 365; i += 1){
+        jsDate.setDate(jsDate.getDate() + 1)
+        const date = jsDateToDashForm(jsDate)
+        outData[date] = {'github': 0, 'gitlab': 0, 'leetcode': 0};
+    }
+    return outData;
+}
+
+
