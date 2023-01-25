79db453ff152c97bb1dad75881f7dcbb75b6c4c7
Connor Crowe <39016118+connorcrowe@users.noreply.github.com>
2023-01-25 14:06:52 -0500

    Update gitignore and delete old testing files

--- toJSON.sh
+++
@@ -1,7 +0,0 @@
-echo "" > output.js
-echo "data = '"  | tr '\n' ' ' >> output.js
-node main.js | tr '\n' ' ' >> output.js 
-echo "'" >> output.js
-#git add output.txt
-#git commit -m "automated output update"
-#git push
