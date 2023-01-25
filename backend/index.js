07f01a4547513ec0c6e3234e7726baa4196e37e6
Connor Crowe <39016118+connorcrowe@users.noreply.github.com>
2023-01-25 14:12:10 -0500

    Potential fix for incorrect port config on Render deployment. Reduce old debug logging server-side

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fetchData = require('./fetchCalendarData.js');
 
-const port = process.env['APP_PORT'] || 3000;
 const app = express();
 
 app.use(express.static('frontend'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/form-submission', async (request, response) => {
-  console.log('Received request: ', request.body);
   const jsonContent = await fetchData.fetchData(request.body);
-  console.log(jsonContent)
   response.send({ ...jsonContent });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(process.env.PORT || 3000);
